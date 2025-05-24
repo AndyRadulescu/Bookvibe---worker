23e499005dc468632aa5a4a589ef1e50eb797c1f
Andy Radulscu <radulescu.eduard.andrei@gmail.com>
2025-05-24 20:03:11 +0300

    remove rabbitMQ (#1)

--- src/main/kotlin/org/example/worker/controller/SearchBookController.kt
+++ src/main/kotlin/org/example/worker/controller/SearchBookController.kt
@@ -1,42 +1,21 @@
 package org.example.worker.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.example.worker.dto.BookDto
-import org.example.worker.dto.IsbnDto
-import org.example.worker.dto.SearchDto
-import org.example.worker.dto.SearchDto.Companion.ISBN
-import org.example.worker.dto.SearchDto.Companion.SEARCH_BOOKS
-import org.example.worker.dto.SearchDto.PatternType
 import org.example.worker.dto.SearchVolumeList
 import org.example.worker.service.SearchBookService
-import org.springframework.amqp.rabbit.annotation.RabbitListener
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
-
-@RestController
+@RestController()
 class SearchBookController(private val bookService: SearchBookService, private val mapper: ObjectMapper) {
-    @GetMapping("/search")
-    fun searchBook(@RequestParam queryParam: String): Flux<SearchVolumeList> {
-        return bookService.searchBook(queryParam)
+    @GetMapping("/worker/books")
+    fun searchBook(@RequestParam name: String, @RequestParam page: Int = 1): Mono<SearchVolumeList> {
+        return bookService.searchBook(name, page)
     }
 
-    @RabbitListener(queues = [SEARCH_BOOKS])
-    fun consumeMessagesSearchBooks(message: SearchDto<BookDto>): SearchVolumeList? {
-        println(message)
-        val searchedVolumeListFlux = bookService.searchBook(message.data.book)
-        return searchedVolumeListFlux.blockFirst()
+    @GetMapping("/worker/volume/{id}")
+    fun searchBookById(@PathVariable id: String): Mono<SearchVolumeList> {
+        return bookService.getBookByVolumeId(id)
     }
-
-    //TODO: change isbn to volume id
-    @RabbitListener(queues = [ISBN])
-    fun consumeMessagesIsbn(message: SearchDto<IsbnDto>): SearchVolumeList? {
-        println(message)
-        val bookMono = bookService.getBookByIsbn(message.data.isbn)
-        return bookMono.block()
-    }
-
 }
